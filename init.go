80b9bf293c9d85cdf9e253c276fb4c0d3c109e6b
jiangwe leo <1099986768@qq.com>
2024-05-13 00:55:44 +0800

    新增版本信息输出

--- init.go
+++ init.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
-var testis = flag.Bool("test", false, "测试")
+var version = flag.Bool("version", false, "测试")
+var (
+	Version   string
+	Branch    string
+	Commit    string
+	BuildTime string
+	GOOS      string
+	GOARCH    string
+)
 
 func init() {
 	flag.Parse()
+	if *version {
+		fmt.Println("|------------------------------ VERSION INFO -----------------------------|")
+		fmt.Printf("VERSION: \t%s\n", Version)
+		fmt.Printf("GOOS: \t\t%s\n", GOOS)
+		fmt.Printf("GOARCH: \t%s\n", GOARCH)
+		fmt.Printf("BRANCH: \t%s\n", Branch)
+		fmt.Printf("COMMIT: \t%s\n", Commit)
+		fmt.Printf("BUILDTIME: \t%s\n", BuildTime)
+		fmt.Println("|-------------------------------------------------------------------------|")
+
+		os.Exit(0)
+	}
 }
