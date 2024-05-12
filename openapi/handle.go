64aec35208e957f9b60694f5a437e95a05f13fb8
jiangwe leo <1099986768@qq.com>
2024-05-13 02:02:10 +0800

    更新确认歌词接口，gitbobobo说只需要返回http状态即可

--- openapi/handle.go
+++ openapi/handle.go
@@ -56,49 +56,51 @@ func LyricInfoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // LyricHandler 歌词信息确认
-// 还未完成
+// 还未完成，gitbobobo说是通知接口而已
 func LyricHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.RequestURI)
-	query := r.URL.Query()
-	title := query.Get("title")
-	artist := query.Get("artist")
-	num, err := strconv.Atoi(query.Get("limit"))
-	//fmt.Println(num)
-	var mj MusicjsonN
-	err = json.Unmarshal([]byte(getSongId(fmt.Sprintf("%v %v", artist, title), fmt.Sprint(num))), &mj)
-	//fmt.Println(err, mj)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if len(mj.MusicSearchSearchCgiService.Data.Body.Song.List) > 0 {
-		if num == 1 {
-			lrc := getLrc(mj.MusicSearchSearchCgiService.Data.Body.Song.List[0].Mid)
-			lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
-			// 写入歌词
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(lrc))
-			return
-		} else {
-			// 返回所有歌词，提供选择
-			var lrcLists []LrcList
-			for _, v := range mj.MusicSearchSearchCgiService.Data.Body.Song.List {
-				lrc := getLrc(v.Mid)
-				lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
-				lrcLists = append(lrcLists, LrcList{
-					Id:     v.Mid,
-					Title:  v.Title,
-					Artist: artist,
-					Lyrics: lrc,
-				})
-			}
-			returnMsg, _ := json.Marshal(lrcLists)
-			w.Header().Set("content-type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write(returnMsg)
-			fmt.Println(string(returnMsg))
-			return
-		}
-	}
-	fmt.Fprint(w, "have no msg")
-	fmt.Println("have no msg")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(""))
+	//fmt.Println(r.RequestURI)
+	//query := r.URL.Query()
+	//title := query.Get("title")
+	//artist := query.Get("artist")
+	//num, err := strconv.Atoi(query.Get("limit"))
+	////fmt.Println(num)
+	//var mj MusicjsonN
+	//err = json.Unmarshal([]byte(getSongId(fmt.Sprintf("%v %v", artist, title), fmt.Sprint(num))), &mj)
+	////fmt.Println(err, mj)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//if len(mj.MusicSearchSearchCgiService.Data.Body.Song.List) > 0 {
+	//	if num == 1 {
+	//		lrc := getLrc(mj.MusicSearchSearchCgiService.Data.Body.Song.List[0].Mid)
+	//		lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
+	//		// 写入歌词
+	//		w.WriteHeader(http.StatusOK)
+	//		w.Write([]byte(lrc))
+	//		return
+	//	} else {
+	//		// 返回所有歌词，提供选择
+	//		var lrcLists []LrcList
+	//		for _, v := range mj.MusicSearchSearchCgiService.Data.Body.Song.List {
+	//			lrc := getLrc(v.Mid)
+	//			lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
+	//			lrcLists = append(lrcLists, LrcList{
+	//				Id:     v.Mid,
+	//				Title:  v.Title,
+	//				Artist: artist,
+	//				Lyrics: lrc,
+	//			})
+	//		}
+	//		returnMsg, _ := json.Marshal(lrcLists)
+	//		w.Header().Set("content-type", "application/json")
+	//		w.WriteHeader(http.StatusOK)
+	//		w.Write(returnMsg)
+	//		fmt.Println(string(returnMsg))
+	//		return
+	//	}
+	//}
+	//fmt.Fprint(w, "have no msg")
+	//fmt.Println("have no msg")
 }
