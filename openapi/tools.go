f157100177a07de75d760e704e8555a7c21914b3
jiangwe leo <1099986768@qq.com>
2024-05-13 00:53:45 +0800

    规范函数名

--- openapi/tools.go
+++ openapi/tools.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func getlrc(songmid string) string {
+func getLrc(songmid string) string {
 	var data Lrcjson
 	s := "https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric.fcg?songmid=" + songmid
 	sss := string(GET(s))
@@ -27,7 +27,7 @@ func getlrc(songmid string) string {
 	return string(ss)
 }
 
-func getsongid(word, num string) string {
+func getSongId(word, num string) string {
 	urlword :=
 		"https://u.y.qq.com/cgi-bin/musicu.fcg?pcachetime=" + strconv.FormatInt(time.Now().UnixMilli(), 10)
 	data := "{\n   \"comm\" : {\n      \"_channelid\" : \"0\",\n      \"_os_version\" : \"6.1.7601-2%2C+Service+Pack+1\",\n      \"authst\" : \"\",\n      \"ct\" : \"19\",\n      \"cv\" : \"1873\",\n      \"guid\" : \"\",\n      \"patch\" : \"118\",\n      \"psrf_access_token_expiresAt\" : 0,\n      \"psrf_qqaccess_token\" : \"\",\n      \"psrf_qqopenid\" : \"\",\n      \"psrf_qqunionid\" : \"\",\n      \"tmeAppID\" : \"qqmusic\",\n      \"tmeLoginType\" : 2,\n      \"uin\" : \"0\",\n      \"wid\" : \"0\"\n   },\n   \"music.search.SearchCgiService\" : {\n      \"method\" : \"DoSearchForQQMusicDesktop\",\n      \"module\" : \"music.search.SearchCgiService\",\n      \"param\" : {\n         \"grp\" : 1,\n         \"num_per_page\" : " + num + ",\n         \"page_num\" : 1,\n         \"query\" : \"" + word + "\",\n         \"remoteplace\" : \"txt.newclient.top\",\n         \"search_type\" : 0,\n         \"searchid\" : \"\"\n      }\n   }\n}"
