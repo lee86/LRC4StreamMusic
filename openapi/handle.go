e886b61511926c5f25f5bf1c2081d2d76e5cd5a0
Jiangwe Leo <15872426870@163.com>
2024-05-20 21:44:34 +0800

    新增缓存判断和存储条件

--- openapi/handle.go
+++ openapi/handle.go
@@ -3,7 +3,9 @@ package openapi
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"qqlrc/lyricCache"
 	"strconv"
 	"strings"
 )
@@ -16,25 +18,47 @@ func LyricInfoHandler(w http.ResponseWriter, r *http.Request) {
 	num, err := strconv.Atoi(query.Get("limit"))
 	if title == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(""))
+		_, err = w.Write([]byte("HAVE NO TITLE"))
+		if err != nil {
+			return
+		}
 		return
 	}
 	var mj MusicjsonN
-	err = json.Unmarshal([]byte(getSongInfo(fmt.Sprintf("%v-%v", artist, title), fmt.Sprint(num))), &mj)
+	keys := fmt.Sprintf("%v-%v", artist, title)
+	if lyric, ok := lyricCache.CacheSelect(keys); ok {
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write([]byte(lyric))
+		if err != nil {
+			return
+		}
+		return
+	}
+	err = json.Unmarshal([]byte(getSongInfo(keys, fmt.Sprint(num))), &mj)
 	//fmt.Println(err, mj)
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(""))
+		_, err = w.Write([]byte(""))
+		if err != nil {
+			return
+		}
 		return
 	}
 	if len(mj.MusicSearchSearchCgiService.Data.Body.Song.List) > 0 {
 		if num == 1 {
-			lrc := getLrc(mj.MusicSearchSearchCgiService.Data.Body.Song.List[0].Mid)
-			lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
+			// 返回1条
+			lrc, ok := lyricCache.CacheSelect(fmt.Sprintf("%v_%v", artist, title))
+			if !ok {
+				lrc = getLrc(mj.MusicSearchSearchCgiService.Data.Body.Song.List[0].Mid)
+				lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
+			}
 			// 写入歌词
 			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(lrc))
+			_, err = w.Write([]byte(lrc))
+			if err != nil {
+				return
+			}
 			return
 		} else {
 			// 返回所有歌词，提供选择
@@ -52,26 +76,47 @@ func LyricInfoHandler(w http.ResponseWriter, r *http.Request) {
 			returnMsg, _ := json.Marshal(lrcLists)
 			w.Header().Set("content-type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			w.Write(returnMsg)
+			_, err = w.Write(returnMsg)
+			if err != nil {
+				return
+			}
 			fmt.Println(string(returnMsg))
 			return
 		}
 	}
 	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(""))
+	_, err = w.Write([]byte(""))
+	if err != nil {
+		return
+	}
 	return
 }
 
 // LyricHandler 歌词信息确认
 // gitbobobo说是通知接口而已
 func LyricHandler(w http.ResponseWriter, r *http.Request) {
-	//data, _ := io.ReadAll(r.Body)
-	//var lyricCheck LyricCheck
-	//json.Unmarshal(data, &lyricCheck)
-
-	//lrc := getLrc(lyricCheck.LyricsId)
-	//lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
-	// 返回200
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	data, _ := io.ReadAll(r.Body)
+	var lyricCheck LyricCheck
+	err := json.Unmarshal(data, &lyricCheck)
+	if err != nil {
+		return
+	}
+	keys := fmt.Sprintf("%v_%v", lyricCheck.Artist, lyricCheck.Title)
+	lrc := getLrc(lyricCheck.LyricsId)
+	lrc = strings.ReplaceAll(lrc, "[by:]", "[by: Jiangwe Leo QQLrc]")
+	if lyricCache.CacheSave(keys, []byte(lrc)) {
+		// 返回200
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("ok"))
+		if err != nil {
+			return
+		}
+		return
+	}
+	// 返回416
+	w.WriteHeader(http.StatusRequestedRangeNotSatisfiable)
+	_, err = w.Write([]byte("check error"))
+	if err != nil {
+		return
+	}
 }
